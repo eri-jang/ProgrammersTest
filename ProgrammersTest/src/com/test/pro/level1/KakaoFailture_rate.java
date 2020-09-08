537258f6392f867e1fcc1091803358d96022c272
Refine <Refine@PC_20140903002>
2020-09-08 13:02:32 +0900

    kakaoTest_2019

--- ProgrammersTest/src/com/test/pro/level1/KakaoFailture_rate.java
+++ ProgrammersTest/src/com/test/pro/level1/KakaoFailture_rate.java
@@ -11,9 +11,22 @@ public class KakaoFailture_rate {
 		for(int i = 0; i < stages.length; i++) {
 			stages[i] = sc.nextInt();
 		}
+		
 		for(int i = 0; i < stages.length; i++) {
-			
 			System.out.print(stages[i] + ",");
+		}
+		System.out.println();
+		
+		int totalUser = stages.length;
+		System.out.println("total :" + totalUser);
+	}
+	
+	
+	//실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 
+	class Solution{
+		public int[] solution(int N, int[]stages) {
+			int[] answer = {};
+			return answer;
 		}
 	}
 
